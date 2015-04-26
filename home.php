12cbb8fed32c81d4920ca89c089beeb0e21d131a
Andy Green <andye.green15@gmail.com>
2015-04-26 15:24:10 -0400

    Start
    Most base pages, Some CSS

---
+++ home.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <title>Bored Gamers Board</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Andy Green">
+        <meta name="description" content="List of links to all of my CS 142 assignemts ">
+
+        <link rel="stylesheet" href="style.css" media="screen">
+
+        <!--[if lt IE 9]>
+            <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    
+    <header>
+        <h1 id="banner">Board Gamers Board</h1>
+    </header>
+ 
+<!--   Nav Bar -->   
+    <nav id="mainNav">
+        <ul>
+            <a href="forum.php"><li class="navButton">Forum</li></a>
+            <a href="calender.php"><li class="navButton">Calender</li></a>
+            <a href="events.php"><li class="navButton">Events</li></a>
+        </ul>
+    </nav>
+   
+<!--   Console Filter buttons -->
+    <aside>
+        <ul>
+            <li class="filter" id="pc">PC</li>
+            <li class="filter" id="360">Xbox 360</li>
+            <li class="filter" id="xbone">Xbox One</li>
+            <li class="filter" id="ps3">PS3</li>
+            <li class="filter" id="ps4">PS4</li>
+        </ul>
+    </aside>
+    
+    <!-- Feed of submissions -->
+    <section id="feed">
+    
+    </section>
+    
+    
+</html>
